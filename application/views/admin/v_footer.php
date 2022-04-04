4a654ddafbf4af38fea1deb4f5236535308a34a5
vadhioram <vadhioram@gmail.com>
2022-04-04 12:51:17 +0700

    dadi

--- application/views/admin/v_footer.php
+++ application/views/admin/v_footer.php
@@ -15,7 +15,7 @@
 <!-- ./wrapper -->
 
 <!-- jQuery -->
-<script src="><?php echo base_url();?>template/admin/plugins/jquery/jquery.min.js"></script>
+<script src="<?php echo base_url();?>template/admin/plugins/jquery/jquery.min.js"></script>
 <!-- jQuery UI 1.11.4 -->
 <script src="<?php echo base_url();?>template/admin/plugins/jquery-ui/jquery-ui.min.js"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
--- application/views/admin/v_header.php
+++ application/views/admin/v_header.php
@@ -8,11 +8,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   <title>AdminLTE 3 | Dashboard</title>
 
   <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="<?php echo base_url();?>template/admin/https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="<?php echo base_url();?>template/admin/plugins/fontawesome-free/css/all.min.css">
   <!-- Ionicons -->
-  <link rel="stylesheet" href="<?php echo base_url();?>template/admin/https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Tempusdominus Bootstrap 4 -->
   <link rel="stylesheet" href="<?php echo base_url();?>template/admin/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
   <!-- iCheck -->
