05b919b65097c616cf52ff7e6c01bbdb854e8497
vadhioram <vadhioram@gmail.com>
2022-04-06 11:44:27 +0700

    n

--- application/views/admin/v_sidebar.php
+++ application/views/admin/v_sidebar.php
@@ -13,7 +13,7 @@
           <img src="<?php echo base_url();?>template/admin/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Ambatukam</a>
+          <a href="#" class="d-block">SISINDUK</a>
         </div>
       </div>
 
