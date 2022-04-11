920b1514c073b140847f1d1bbfc481f7efabc853
fzlfade <faizal210505@gmail.com>
2022-04-11 08:45:40 +0700

    niggs

---
+++ application/views/landing/v_footer.php
@@ -0,0 +1,10 @@
+    <script src="<?php echo base_url();?>template/landing/js/jquery.min.js"></script> <!-- jQuery for Bootstrap's JavaScript plugins -->
+    <script src="<?php echo base_url();?>template/landing/js/popper.min.js"></script> <!-- Popper tooltip library for Bootstrap -->
+    <script src="<?php echo base_url();?>template/landing/js/bootstrap.min.js"></script> <!-- Bootstrap framework -->
+    <script src="<?php echo base_url();?>template/landing/js/jquery.easing.min.js"></script> <!-- jQuery Easing for smooth scrolling between anchors -->
+    <script src="<?php echo base_url();?>template/landing/js/swiper.min.js"></script> <!-- Swiper for image and text sliders -->
+    <script src="<?php echo base_url();?>template/landing/js/jquery.magnific-popup.js"></script> <!-- Magnific Popup for lightboxes -->
+    <script src="<?php echo base_url();?>template/landing/js/validator.min.js"></script> <!-- Validator.js - Bootstrap plugin that validates forms -->
+    <script src="<?php echo base_url();?>template/landing/js/scripts.js"></script> <!-- Custom scripts -->
+</body>
+</html>
