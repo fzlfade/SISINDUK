19a0ef330c3210b712b6473fbbd7aba47a1e8992
vadhioram <vadhioram@gmail.com>
2022-04-12 09:44:43 +0700

    sad

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -39,17 +39,5 @@ class User extends CI_Controller {
 		// mengalihkan halaman ke halaman data petugas
 		redirect(base_url().'user');
 
-	}
-
-	function user_edit($id){
-		$where = array('id' => $id);
-		// mengambil data dari database sesuai id
-		$data['tbl_user'] = $this->m_data->edit_data($where,'tbl_user')->result();
-		$this->load->view('admin/v_header');
-		$this->load->view('admin/v_navbar');
-		$this->load->view('admin/v_sidebar');
-		$this->load->view('admin/v_user_edit',$data);
-		$this->load->view('admin/v_footer');
-
-	}
+	}	
 }
